00cd515a0b31abf3afcd30d9b0992891e2a76bbe
Gerald López G <64161345+gerald-lg@users.noreply.github.com>
2024-09-17 08:41:40 -0300

    fix: undefined

--- auth.ts
+++ auth.ts
@@ -28,7 +28,13 @@ export const { auth, signIn, signOut } = NextAuth({
           if (parsedCredentials.success) {
             const { email, password } = parsedCredentials.data;
             const user = await getUser(email);
-            const passwordsMatch = await bcrypt.compare(password, user!.password);
+
+            if (!user) {
+              console.log('User not found');
+              return null;
+            }
+
+            const passwordsMatch = await bcrypt.compare(password, user.password);
   
             if (passwordsMatch) return user;
           }
